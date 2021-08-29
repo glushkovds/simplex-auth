576908bc75d21730f4ea8094c37744b3e69108a9
Denis Glushkov <amkarovec@gmail.com>
2021-08-29 17:15:25 +0500

    WIP

---
+++ src/Auth/BasicAuthMiddleware.php
@@ -0,0 +1,31 @@
+<?php
+
+
+namespace Simplex\Auth\Auth;
+
+use Simplex\Core\Identity\Models\User;
+use Simplex\Core\Middleware\Handler;
+
+class BasicAuthMiddleware implements Handler
+{
+
+    /**
+     * @inheritDoc
+     */
+    public function handle($payload, \Closure $next)
+    {
+        if ($payload) {
+            return $next($payload);
+        }
+        $login = $_SERVER['PHP_AUTH_USER'] ?? null;
+        $pass = $_SERVER['PHP_AUTH_PW'] ?? null;
+        if ($login && $pass) {
+            $user = User::findOne(['login' => $login, 'password' => md5($pass)]);
+            if ($user) {
+                return $next($user);
+            }
+        }
+        return $next($payload);
+    }
+
+}
